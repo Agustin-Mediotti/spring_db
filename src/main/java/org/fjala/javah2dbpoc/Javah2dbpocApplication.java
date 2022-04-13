527b88c6b92203823c64c83552182983164a2ab1
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-04-13 12:55:46 -0300

    first commit

---
+++ src/main/java/org/fjala/javah2dbpoc/Javah2dbpocApplication.java
@@ -0,0 +1,13 @@
+package org.fjala.javah2dbpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javah2dbpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javah2dbpocApplication.class, args);
+	}
+
+}
